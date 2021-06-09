d68662c5ebb9440082e1648b052fe4bfa30aca82
meysamrsl <84845559+meysamrsl@users.noreply.github.com>
2021-06-10 02:32:35 +0430

    Update mashghe shab.py

--- mashghe shab.py
+++ mashghe shab.py
@@ -23,3 +23,65 @@ def calculator(division):
         print(f'division is: {division}')
 division = num1 / num2
 calculator(division)
+
+
+###############################################################
+
+
+command = input('che shekli mikhay?:\n')
+if command == 'mosalas':
+    def Perimeter_Area(mohit_mosalas, masahat_mosalas):
+        if command2 == 'mohit':
+            print(f'mohit mosavi ast ba: {mohit_mosalas}')
+        elif command2 == 'masahat':
+            print(f'masahat mosavi ast ba: {masahat_mosalas}')
+        else:
+            print('not found')
+    command2 = input('mohit ya masahat?\n')
+    ghaede = float(input('ghaede ra vared konid\n'))
+    ertefa = float(input('ertefa ra vared konid\n'))
+    mohit_mosalas = ghaede * 3
+    masahat_mosalas = (ghaede * ertefa) / 2
+    Perimeter_Area(mohit_mosalas, masahat_mosalas)
+elif command == 'mostatil':
+    def Perimeter_Area(mohit_mostatil, masahat_mostatil):
+        if command2 == 'mohit':
+            print(f'mohit mosavi ast ba: {mohit_mostatil}')
+        elif command2 == 'masahat':
+            print(f'masahat mosavi ast ba: {masahat_mostatil}')
+        else:
+            print('not found')
+    command2 = input('mohit ya masahat?\n')
+    tol = float(input('tol ra vared konid\n'))
+    arz = float(input('arz ra vared konid\n'))
+    mohit_mostatil = (tol + arz) * 2
+    masahat_mostatil = tol * arz
+    Perimeter_Area(mohit_mostatil, masahat_mostatil)
+elif command == 'dayere':
+        def Perimeter_Area(mohit_dayere, masahat_dayere):
+            if command2 == 'mohit':
+                print(f'mohit mosavi ast ba: {mohit_dayere}')
+            elif command2 == 'masahat':
+                print(f'masahat mosavi ast ba: {masahat_dayere}')
+            else:
+                print('not found')
+        command2 = input('mohit ya masahat?\n')
+        shoa = float(input('shoa ra vared konid\n'))
+        mohit_dayere = (shoa * 2) * 3.14
+        masahat_dayere = (shoa ** 2) * 3.14
+        Perimeter_Area(mohit_dayere, masahat_dayere)
+elif command == 'moraba':
+    def Perimeter_Area(mohit_moraba, masahat_moraba):
+        if command2 == 'mohit':
+            print(f'mohit mosavi ast ba: {mohit_moraba}')
+        elif command2 == 'masahat':
+            print(f'masahat mosavi ast ba: {masahat_moraba}')
+        else:
+            print('not found')
+    command2 = input('mohit ya masahat?\n')
+    zele_moraba = float(input('zele moraba ra vared konid\n'))
+    mohit_moraba = zele_moraba * 4
+    masahat_moraba = zele_moraba ** 2
+    Perimeter_Area(mohit_moraba, masahat_moraba)
+else:
+    print('not found')
